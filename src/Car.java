6772763d658898f165c151683a8cb4d5d9ace45e
bhubhik <bhubhik@gmail.com>
2023-09-19 20:13:27 +1000

    create car class

---
+++ src/Car.java
@@ -0,0 +1,31 @@
+public class Car {
+  private String make;
+  private String model;
+  private int year;
+  private boolean isRunning;
+
+  public Car(String make, String model, int year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.isRunning = false;
+  }
+
+  public void start() {
+    if (!isRunning) {
+      isRunning = true;
+      System.out.println(make + " " + model + " is started.");
+    } else {
+      System.out.println(make + " " + model + " is already running.");
+    }
+  }
+
+  public void stop() {
+    if (isRunning) {
+      isRunning = false;
+      System.out.println(make + " " + model + " is stopped.");
+    } else {
+      System.out.println(make + " " + model + " is already stopped.");
+    }
+  }
+}
