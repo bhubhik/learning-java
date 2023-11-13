50e1cf2c39ad03176e5b9cddd061d33118be8a4d
bhubhik <bhubhik@gmail.com>
2023-11-14 09:30:11 +1100

    play again in guessing game

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -4,25 +4,32 @@ import javax.swing.JOptionPane;
 public class GuessingGame {
   public static void main(String[] args) {
     String gameName = "Guessing Game";
-    int computerNumber = (int) (Math.random() * 20 + 1);
-    int userAnswer = 0;
-    int count = 1;
 
-    while (userAnswer != computerNumber) {
-      String response = JOptionPane.showInputDialog(null, "Enter your guess between 1-20", gameName, 3);
+    boolean playAgain = true;
 
-      try {
-        userAnswer = Integer.parseInt(response);
-        if (userAnswer < 1 || userAnswer > 20) {
-          JOptionPane.showMessageDialog(null, "Invalid guess. Number should be between 1 to 20.", gameName, 0);
-        } else {
-          count++;
-          String result = checkGuessedNumber(userAnswer, computerNumber, count);
-          JOptionPane.showMessageDialog(null, "" + result, gameName, 1);
+    while (playAgain) {
+      int computerNumber = (int) (Math.random() * 20 + 1);
+      int userAnswer = 0;
+      int count = 1;
+
+      while (userAnswer != computerNumber) {
+        String response = JOptionPane.showInputDialog(null, "Enter your guess between 1-20", gameName, 3);
+
+        try {
+          userAnswer = Integer.parseInt(response);
+          if (userAnswer < 1 || userAnswer > 20) {
+            JOptionPane.showMessageDialog(null, "Invalid guess. Number should be between 1 to 20.", gameName, 0);
+          } else {
+            count++;
+            String result = checkGuessedNumber(userAnswer, computerNumber, count);
+            JOptionPane.showMessageDialog(null, "" + result, gameName, 1);
+          }
+        } catch (NumberFormatException e) {
+          JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.", gameName, 0);
         }
-      } catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number.", gameName, 0);
       }
+      String playAgainResponse = JOptionPane.showInputDialog(null, "Play again?(yes/no)", gameName, 3);
+      playAgain = "yes".equalsIgnoreCase(playAgainResponse);
     }
   }
 
